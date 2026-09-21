d4f408ce9e0f0c41999234ced464de360349852a
deena-shkeir <dws9050@uncw.edu>
2026-09-21 16:42:00 -0400

    Add files via upload

---
+++ prob set 4.R
@@ -0,0 +1,208 @@
+#q8 
+EX = 7
+a = 5
+b = 5
+a*EX+b
+
+#q9
+EX = 3
+EY = 6
+a = 3
+b = 2
+a*EX + b*EY
+
+#q10
+EX = 3
+EY = 8
+a = 3
+b = 5
+c = 3
+a*EX + b*EY - c
+
+#q11
+EX = 4
+EX2 = 52  
+EX2 - (EX)^2  
+
+#q12
+VarX = 4
+a = 5
+5^2 * 4
+
+#q13
+VarX = 23
+sqrt(VarX)
+
+#q14
+VarX = 6
+VarY = 5
+VarX+VarY
+
+#q15
+EXY = 7
+EX = 4
+EY = 6
+EXY - 4*6
+
+#q16
+x = c(3,2,5)
+y = c(3,2,0)
+p = c(1/3,1/3,1/3)
+EXY = sum(x*y*p)
+EX = sum(x*p)
+EY = sum(y*p)
+EXY - EX*EY
+
+#q17
+Cov = -1
+a1 = 5
+a2 = 4
+a1*a2*Cov
+
+#q18
+Cov = 4
+sdx = 4
+sdy = 4
+4/(4*4)
+
+#q19
+Varx = 6
+VarY = 6
+Cov = 2
+a = 1
+b = 1
+a^2*VarX + b^2*VarY + 2*a*b*Cov
+
+#q20
+y = c(11,4,10,10)
+mean(y)
+
+#q21
+y = c(2,7,3)
+p = c(0.2,0.3,0.5)
+sum(y*p)
+
+#q22
+E = 3
+a = 5
+b = 1
+E*a+b
+
+
+##attempt 2
+
+#8
+EX = 5
+a = 5
+b = 4
+a*EX+b
+
+#9
+EX = 4
+EY = 3
+a = 5
+b = 2
+a*EX+b*EY
+
+#10
+EX = 5
+EY = 8
+a = 5
+b = 3
+c = 5
+a*EX+b*EY-c
+
+#11
+EX = 6
+EX2 = 42
+EX2 - EX^2
+
+#12
+VarX = 5
+a = 2
+a^2*VarX
+
+#13
+VarX = 18
+sqrt(VarX)
+
+#14
+varx = 6
+vary = 4
+varx+vary
+
+#15
+EXY = 26
+EX = 4
+EY = 5
+EXY - EX*EY
+
+#16
+x = c(3,0,0)
+y = c(3,2,0)
+p = c(1/3,1/3,1/3)
+EXY = sum(x*y*p)
+EX = sum(x*p)
+EY = sum (y*p)
+EXY - EX*EY
+
+#17
+cov = -1
+a1 = 2
+a2 = 3
+a1*a2*cov
+
+#18
+cov = 0
+sdx = 2
+sdy= 2
+cov/(sdx*sdy)
+
+#19 
+varx = 3
+vary = 4
+cov = -2
+a = 2
+b = 3
+a^2*varx+b^2*vary+2*a*b*cov
+
+#20
+y = c(8,5,0,11)
+mean(y)
+
+#21
+y = c(3,0,9)
+p = c(0.2,0.3,0.5)
+sum(y*p)
+
+#22
+EYX = 4
+a = 2
+b = 2
+a*EYX + b  
+  
+
+#10 on quiz parallelogram identity Var(aX+bY)
+varx = 4
+vary = 2
+covxy = 0 
+a = 1
+b = 3
+answer = a^2*varx + b^2*vary + 2*a*b*covxy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
